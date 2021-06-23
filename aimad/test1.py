7b5c8ab0047859003e878f437c17442dd595637c
aimad1234 <aimadelhammdani@gmail.com>
2021-06-23 15:10:45 +0100

    ggggggggg

--- aimad/test1.py
+++ aimad/test1.py
@@ -7,13 +7,14 @@ Created on Mon Jun 21 16:49:09 2021
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
-from kafka import KafkaProducer
+from kafka import KafkaProducer,KafkaConumer
 import json
 
-access_token = "1226574939119136769-Fdpz42OY4GrxOW7JzOFw3DLkF1WMdXv"
-access_token_secret =  "2DlTX9RZkZ1YarSISmVfNnmVpboDkRjvrWQNTNBlmF8gw"
-api_key =  "mVTsmXLwbVztsmIqJmZ52vWAW"
-api_secret =  "ToiUNpdHUpr7q0ueRZ0CBvCkKmUBC9C9NjlzBcQ41b0oYx1Uon"
+
+access_token="1226574939119136769-Fdpz42OY4GrxOW7JzOFw3DLkF1WMdXv"
+access_token_secret = "2DlTX9RZkZ1YarSISmVfNnmVpboDkRjvrWQNTNBlmF8gw"
+api_key="mVTsmXLwbVztsmIqJmZ52vWAW"
+api_secret="ToiUNpdHUpr7q0ueRZ0CBvCkKmUBC9C9NjlzBcQ41b0oYx1Uon"
 class StdOutListener(StreamListener):
     def on_data(self, data):
         json_ = json.loads(data) 
