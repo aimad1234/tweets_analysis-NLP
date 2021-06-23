fbc13b12802cabe219708c675b29c9fdfdcbfa5b
Moody <mehdichellak47@gmail.com>
2021-06-23 15:08:16 +0100

    cccc

--- aimad/test1.py
+++ aimad/test1.py
@@ -10,10 +10,10 @@ from tweepy import Stream
 from kafka import KafkaProducer
 import json
 
-access_token = "1226574939119136769-Fdpz42OY4GrxOW7JzOFw3DLkF1WMdXv"
-access_token_secret =  "2DlTX9RZkZ1YarSISmVfNnmVpboDkRjvrWQNTNBlmF8gw"
-api_key =  "mVTsmXLwbVztsmIqJmZ52vWAW"
-api_secret =  "ToiUNpdHUpr7q0ueRZ0CBvCkKmUBC9C9NjlzBcQ41b0oYx1Uon"
+access_token = "1226574939119136769-bd5aBC0QhzE7Qp2GOWULD8CGAyeisd"
+access_token_secret =  "8yt5u3d2JMPd8ZgBTy9MoA34I8GYnumQP3b9EZ7sExQtx"
+api_key =  "vvl6tAUpTVYG2p9XrrY3pGdS3"
+api_secret =  "kv3K5vj9DyTiYB7ELZfgxANrZOZIxlZ29KwKDok4rBo2WHkCyI"
 class StdOutListener(StreamListener):
     def on_data(self, data):
         json_ = json.loads(data) 
@@ -22,9 +22,9 @@ class StdOutListener(StreamListener):
     def on_error(self, status):
         print (status)
 
-producer = KafkaProducer(bootstrap_servers='localhost:9092')
+producer = KafkaProducer(bootstrap_servers='192.168.1.8:9092')
 l = StdOutListener()
 auth = OAuthHandler(api_key, api_secret)
 auth.set_access_token(access_token, access_token_secret)
 stream = Stream(auth, l)
-stream.filter(track=["vaccins", "corona virus"])
+stream.filter(track=["trump"],languages = ['fr'])
