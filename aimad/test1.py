0b7b773b38151edf5df781eb93895a611c2c164d
aimad1234 <aimadelhammdani@gmail.com>
2021-06-23 15:50:08 +0100

    code final

--- aimad/test1.py
+++ aimad/test1.py
@@ -10,10 +10,6 @@ from tweepy import Stream
 from kafka import KafkaProducer
 import json
 
-
-
-
-
 access_token = "1226574939119136769-bd5aBC0QhzE7Qp2GOWULD8CGAyeisd"
 access_token_secret =  "8yt5u3d2JMPd8ZgBTy9MoA34I8GYnumQP3b9EZ7sExQtx"
 api_key =  "vvl6tAUpTVYG2p9XrrY3pGdS3"
@@ -22,15 +18,15 @@ api_secret =  "kv3K5vj9DyTiYB7ELZfgxANrZOZIxlZ29KwKDok4rBo2WHkCyI"
 class StdOutListener(StreamListener):
     def on_data(self, data):
         json_ = json.loads(data) 
-        producer.send("jattioui", json_["text"].encode('utf-8'))
+        producer.send("quickstart-events", json_["text"].encode('utf-8'))
         return True
     def on_error(self, status):
         print (status)
 
 
-producer = KafkaProducer(bootstrap_servers='192.168.1.21')
+producer = KafkaProducer(bootstrap_servers='192.168.1.20:9092')
 
-producer = KafkaProducer(bootstrap_servers='192.168.1.8:9092')
+
 
 l = StdOutListener()
 auth = OAuthHandler(api_key, api_secret)
